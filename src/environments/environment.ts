70bfb27ca4255322a020584ec350fcec3a5a0db5
PVANH <anhpv1602@gmail.com>
2023-09-05 08:48:29 +0700

    edit environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     projectId: 'noteopportunity',
     appId: '1:812132280946:web:35eb39ce5ed42f8be930eb',
     storageBucket: 'noteopportunity.appspot.com',
-    apiKey: 'edit with real key',
+    apiKey: 'AIzaSyBrcfn3e4uQjBbilt332QOlWKJXwstSSvA',
     authDomain: 'noteopportunity.firebaseapp.com',
     messagingSenderId: '812132280946',
     measurementId: 'G-6629GXRQ9B',
