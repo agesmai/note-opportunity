2baeab4c155ed6e4a9ccb53fef0a5976079ac9de
PVANH <anhpv1602@gmail.com>
2023-09-05 09:26:28 +0700

    change gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'noteopportunity',
+    appId: '1:812132280946:web:35eb39ce5ed42f8be930eb',
+    storageBucket: 'noteopportunity.appspot.com',
+    apiKey: 'AIzaSyBrcfn3e4uQjBbilt332QOlWKJXwstSSvA',
+    authDomain: 'noteopportunity.firebaseapp.com',
+    messagingSenderId: '812132280946',
+    measurementId: 'G-6629GXRQ9B',
+  },
+};
