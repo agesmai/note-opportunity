7d54bc11742b49751655ca45738030d7f855f6e0
PVANH <anhpv1602@gmail.com>
2023-09-07 17:50:39 +0700

    Fix sidebar-nav

--- src/app/layout/sidebar/_nav.ts
+++ src/app/layout/sidebar/_nav.ts
@@ -1,58 +1,58 @@
-// import { INavData } from '@coreui/angular';
-interface INavAttributes {
-  [propName: string]: any;
-}
+import { INavData } from '@coreui/angular';
+// interface INavAttributes {
+//   [propName: string]: any;
+// }
+//
+// interface INavWrapper {
+//   attributes: INavAttributes;
+//   element: string;
+// }
+//
+// interface INavBadge {
+//   text: string;
+//   color: string;
+//   size?: string;
+//   class?: string;
+// }
+//
+// interface INavLabel {
+//   class?: string;
+//   variant: string;
+// }
+//
+// interface INavLinkProps {
+//   queryParams?: {[k: string]: any};
+//   fragment?: string;
+//   queryParamsHandling?: 'merge' | 'preserve' | '';
+//   preserveFragment?: boolean;
+//   skipLocationChange?: boolean;
+//   replaceUrl?: boolean;
+//   state?: {[k: string]: any};
+//   routerLinkActiveOptions?: {exact: boolean};
+//   routerLinkActive?: string | string[];
+// }
 
-interface INavWrapper {
-  attributes: INavAttributes;
-  element: string;
-}
-
-interface INavBadge {
-  text: string;
-  color: string;
-  size?: string;
-  class?: string;
-}
-
-interface INavLabel {
-  class?: string;
-  variant: string;
-}
-
-interface INavLinkProps {
-  queryParams?: {[k: string]: any};
-  fragment?: string;
-  queryParamsHandling?: 'merge' | 'preserve' | '';
-  preserveFragment?: boolean;
-  skipLocationChange?: boolean;
-  replaceUrl?: boolean;
-  state?: {[k: string]: any};
-  routerLinkActiveOptions?: {exact: boolean};
-  routerLinkActive?: string | string[];
-}
-
-interface INavData {
-  name?: string;
-  url?: string | any[];
-  href?: string;
-  icon?: string;
-  iconComponent?: any;
-  badge?: INavBadge;
-  title?: boolean;
-  children?: INavData[];
-  variant?: string;
-  attributes?: INavAttributes;
-  divider?: boolean;
-  class?: string;
-  label?: INavLabel;
-  wrapper?: INavWrapper;
-  linkProps?: INavLinkProps;
-}
+// interface INavData {
+//   name?: string;
+//   url?: string | any[];
+//   href?: string;
+//   icon?: string;
+//   iconComponent?: any;
+//   badge?: INavBadge;
+//   title?: boolean;
+//   children?: INavData[];
+//   variant?: string;
+//   attributes?: INavAttributes;
+//   divider?: boolean;
+//   class?: string;
+//   label?: INavLabel;
+//   wrapper?: INavWrapper;
+//   linkProps?: INavLinkProps;
+// }
 
 export const navItems: INavData[] = [
   {
-    name: 'Dashboard',
+    name: 'Timeline',
     url: '/app/dashboard',
     icon: 'fab fa-windows'
   },
