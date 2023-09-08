c619e073e256e251a907a9be2aefc04af1ae52f7
PVANH <anhpv1602@gmail.com>
2023-09-08 17:48:04 +0700

    minor changes

--- src/app/components/organization-map/organization-map.component.ts
+++ src/app/components/organization-map/organization-map.component.ts
@@ -11,300 +11,306 @@ export class OrganizationMapComponent {
   data: TreeNode[] = [
     {
       expanded: true,
-      type: 'person',
-      data: {
-        image: 'assets/img/avatars/simson.jpg',
-        name: 'Nguyen Viet Anh',
-        title: 'Giam doc Trung tam',
-        department: 'Board of Manager'
-      },
+      label: 'HAS',
       children: [
-
         {
           expanded: true,
-          label: 'HAS HN',
+          type: 'person',
+          data: {
+            image: 'assets/img/avatars/simson.jpg',
+            name: 'Nguyen Viet Anh',
+            title: 'Giam doc Trung tam',
+            department: 'Board of Manager'
+          },
           children: [
+
             {
               expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Pham Viet Anh',
-                title: 'Truong BP',
-                department: 'Presales'
-              }
-            },
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Tran Xuan Quynh',
-                title: 'Truong phong',
-                department: 'Sales'
-              },
+              label: 'HAS HN',
               children: [
                 {
-                  type: 'team',
+                  expanded: true,
+                  type: 'person',
                   data: {
-                    department: 'Sales team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'AM',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'AM',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Pham Viet Anh',
+                    title: 'Truong BP',
+                    department: 'Presales'
                   }
                 },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Tran Xuan Quynh',
+                    title: 'Truong phong',
+                    department: 'Sales'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'Sales team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'AM',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'AM',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
 
+                  ]
+                },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Tran Nam Tien',
+                    title: 'Truong phong',
+                    department: 'R&D'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'R&D team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
+
+                  ]
+                },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Nguyen Quoc Duy',
+                    title: 'Truong phong',
+                    department: 'PMO'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'R&D team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
+
+                  ]
+                }
               ]
             },
             {
               expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Tran Nam Tien',
-                title: 'Truong phong',
-                department: 'R&D'
-              },
+              label: 'HAS HCM',
               children: [
                 {
-                  type: 'team',
+                  expanded: true,
+                  type: 'person',
                   data: {
-                    department: 'R&D team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Pham Viet Anh',
+                    title: 'Truong BP',
+                    department: 'Presales'
                   }
                 },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Tran Xuan Quynh',
+                    title: 'Truong phong',
+                    department: 'Sales'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'Sales team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
 
+                  ]
+                },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Tran Nam Tien',
+                    title: 'Truong phong',
+                    department: 'R&D'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'R&D team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
+
+                  ]
+                },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Nguyen Quoc Duy',
+                    title: 'Truong phong',
+                    department: 'PMO'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'R&D team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
+
+                  ]
+                },
+                {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                    image: 'assets/img/avatars/simson.jpg',
+                    name: 'Nguyen Quoc Duy',
+                    title: 'Truong phong',
+                    department: 'PMO'
+                  },
+                  children: [
+                    {
+                      type: 'team',
+                      data: {
+                        department: 'R&D team',
+                        team: [
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Nguyen Viet An',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Chu Thi Ngoc',
+                            title: 'Sale',
+                          },
+                          {
+                            image: 'assets/img/avatars/simson.jpg',
+                            name: 'Pham Thi My Linh',
+                            title: 'Administrator',
+                          }
+                        ],
+                      }
+                    },
+
+                  ]
+                }
               ]
             },
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Nguyen Quoc Duy',
-                title: 'Truong phong',
-                department: 'PMO'
-              },
-              children: [
-                {
-                  type: 'team',
-                  data: {
-                    department: 'R&D team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
-                  }
-                },
-
-              ]
-            }
           ]
-        },
-        {
-          expanded: true,
-          label: 'HAS HCM',
-          children: [
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Pham Viet Anh',
-                title: 'Truong BP',
-                department: 'Presales'
-              }
-            },
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Tran Xuan Quynh',
-                title: 'Truong phong',
-                department: 'Sales'
-              },
-              children: [
-                {
-                  type: 'team',
-                  data: {
-                    department: 'Sales team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
-                  }
-                },
-
-              ]
-            },
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Tran Nam Tien',
-                title: 'Truong phong',
-                department: 'R&D'
-              },
-              children: [
-                {
-                  type: 'team',
-                  data: {
-                    department: 'R&D team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
-                  }
-                },
-
-              ]
-            },
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Nguyen Quoc Duy',
-                title: 'Truong phong',
-                department: 'PMO'
-              },
-              children: [
-                {
-                  type: 'team',
-                  data: {
-                    department: 'R&D team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
-                  }
-                },
-
-              ]
-            },
-            {
-              expanded: true,
-              type: 'person',
-              data: {
-                image: 'assets/img/avatars/simson.jpg',
-                name: 'Nguyen Quoc Duy',
-                title: 'Truong phong',
-                department: 'PMO'
-              },
-              children: [
-                {
-                  type: 'team',
-                  data: {
-                    department: 'R&D team',
-                    team: [
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Nguyen Viet An',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Chu Thi Ngoc',
-                        title: 'Sale',
-                      },
-                      {
-                        image: 'assets/img/avatars/simson.jpg',
-                        name: 'Pham Thi My Linh',
-                        title: 'Administrator',
-                      }
-                    ],
-                  }
-                },
-
-              ]
-            }
-          ]
-        },
+        }
       ]
-    }
+    },
   ];
 }
